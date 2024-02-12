05c6ec3aa86050e26b241b7d76a53d9d7d7d4020
Crowdbotics <team@crowdbotics.com>
2024-02-12 11:42:06 +0000

    Initial commit

---
+++ backend/stealthtest_1_46253/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for stealthtest_1_46253 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "stealthtest_1_46253.settings"
+)
+
+application = get_wsgi_application()
